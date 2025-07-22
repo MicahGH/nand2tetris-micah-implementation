64eaa7fe2ded8517cc59b5f779af57eda7a03471
MicahGH <miicah.shiiels@gmail.com>
2025-07-22 20:46:51 +0200

    Implement command for "if-goto"

--- projects/07_and_08/vm_translator/models/branching.py
+++ projects/07_and_08/vm_translator/models/branching.py
@@ -9,6 +9,8 @@ class BranchingCommandType(BaseCommandType):
 
 class BaseBranchingCommand(BaseCommand):
     command: BranchingCommandType  # type: ignore[reportGeneralTypeIssues]
+    # Branching command specifiers cannot be Enums as they are user-defined
+    command_specifier: str # type: ignore[reportGeneralTypeIssues]
     # This is never needed in branching commands
     # so it is set to always be None
     command_value: None  # type: ignore[reportGeneralTypeIssues]
@@ -16,12 +18,36 @@ class BaseBranchingCommand(BaseCommand):
 
 class LabelCommand(BaseBranchingCommand):
     def translate_to_asm(self):
-        create_label = f"({self.command_specifier})"
+        create_label = f"""
+        ({self.command_specifier})
+        """
         return [create_label]
 
 
-class GotoCommand(BaseBranchingCommand):
+class GoToCommand(BaseBranchingCommand):
     def translate_to_asm(self):
-        address_label = f"@{self.command_specifier}"
-        create_goto = "0;JMP"
+        address_label = f"""
+        @{self.command_specifier}
+        """
+        create_goto = """
+        0;JMP
+        """
         return [address_label + create_goto]
+
+class IfGoToCommand(BaseBranchingCommand):
+    def translate_to_asm(self):
+        decrement_pointer_addr = """
+        @SP
+        M=M-1
+        """
+        get_value_in_pointer = """
+        A=M
+        D=M
+        """
+        address_label = f"""
+        @{self.command_specifier}
+        """
+        create_if_goto = """
+        D;JNE
+        """
+        return [decrement_pointer_addr + get_value_in_pointer + address_label + create_if_goto]
